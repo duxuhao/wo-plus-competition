d32fc235d00de68a9d9b42f06dbe782bad63ede5
Xuhao Du <21644336@student.uwa.edu.au>
2016-04-14 13:49:26 +0800

    label

--- Q2/read.py
+++ Q2/read.py
@@ -6,6 +6,7 @@ Created on Sat Apr 09 13:53:39 2016
 """
 # -*- coding: utf-8 -*-
 import pandas as pd
+import numpy as np
 Month = ["01","02","03","04","05","06","07","08","09","10","11","12"]
 df = pd.DataFrame()
 for month in Month:
@@ -15,5 +16,12 @@ for month in Month:
 
 df.columns = ['Month', 'IMSI','Network','Gender','Age','APRU','Brand','Model','Flow','Call','SMS']
 df = df.sort_values(['IMSI','Month'])
-df.Model = map(str.lower,df.Model)
-df.Brand = map(str.lower,df.Brand)
+#df.Model = map(str.lower,df.Model)
+#df.Brand = map(str.lower,df.Brand)
+#df['Label'] = pd.Series(np.zeros(len(df)),index=df.index)
+label = pd.Series(np.zeros(len(df)))
+for i, model in enumerate(df.Model):
+    #if (i+1) % 10 & (i+1) % 11 & (i+1) % 12:
+    if 1-int(model == df[i+3:i+4].Model):
+        label[i]= 1
+        print i
